cae7c0bf6cc26a8c1912b3328f4856f975a87112
KrystianOg <62177596+KrystianOg@users.noreply.github.com>
2021-03-31 00:23:56 +0200

    added distupter
    +disruption method 1
    +disruption method 2
    changed a few things
    considering adding enum for voltage 'V' 'B' 'H' 'L' 'Z'

---
+++ Project Files/Disruptor.py
@@ -0,0 +1,27 @@
+import math
+import random
+import Sygnal
+
+
+
+sygnal = Sygnal.Sygnal.__init__([0,1,0,1,1,1,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1])
+
+
+
+class Disruptor:
+    def distruption(signal,algorithm):
+        #prawdopodobieństwo początkowe? 
+        p=0.2
+        distruptedsignal=signal
+
+        #lista indeksów tych samych znaków
+        be_index = []
+        #miejsca zakłóceń
+        dis_index = []
+
+        i=0
+
+        for x in distruptedsignal.voltage:
+            printf('a')
+
+       
