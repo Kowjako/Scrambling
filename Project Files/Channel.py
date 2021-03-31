fde32a3b2c05bfc1cd4c4ed022a688e2e81e8813
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-03-31 22:07:42 +0200

    Add final realization of transmission channel

--- Project Files/Channel.py
+++ Project Files/Channel.py
@@ -2,6 +2,7 @@ from Disruptor import Disruptor
 from Scrambler import Scrambler
 import Receiver
 from Descrambler import Descrambler
+
 #Klasa przedstawiajaca kanal transmisyjny dla sygnalu scramblowanego/niescramblowanego
 class Channel:
     def __init__(self, sygnal, algorythm):
@@ -11,17 +12,14 @@ class Channel:
         
         signal1 = disrupter.disruption2(sygnal, algorythm)  #zaklocenie sygnalu bez scramblowania
         
-        signal2 = scrambler.scramble(algorythm)  #wykonanie scramblingu na sygnale zadanym algorytmem
+        signal2 = scrambler.scramble(algorythm).getScrambledSignal()  #wykonanie scramblingu na sygnale zadanym algorytmem
         
-        disrupter2 = Disruptor(signal2.getDescrambledSygnal()) #utworzenie disruptora na podstawie skramlowanego sygnalu
+        disrupter2 = Disruptor(signal2) #utworzenie disruptora na podstawie skramlowanego sygnalu
         
         signal3 = disrupter2.disruption(algorythm) #zaklocenie skramblowanego sygnlau
         
         descram = Descrambler(signal3,algorythm) #tworzenie descramblera na podstawie zakloconego skramblowanego sygnalu
         
         signal4 = descram.getDescrambledSygnal() #descramblowanie sygnalu
-        
-        tab1 = list(Receiver.signalhistogram(signal2).keys())
-        tab2 = list(Receiver.signalhistogram(signal2).values())
-        
-        Receiver.receiver(sygnal, signal1, signal4, algorythm, signal2, signal3, tab1, tab2)
+            
+        Receiver.receiver(sygnal, signal1, signal4, algorythm, signal2, signal3) #wysylka wszystich sygnalow
