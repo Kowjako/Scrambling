9ab02d8c394475d7fa9d82959a9c8f926ce97e09
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-04-01 13:57:09 +0200

    Small descrambler fixes

--- Project Files/Descrambler.py
+++ Project Files/Descrambler.py
@@ -9,13 +9,13 @@ class Descrambler:
             while i < len(self.descrambledsygnal.signal) - 3:  #ze wzgledu ze maska wyglada B00V lub 000V wiec ostatnie sprawdzenie bedzie na dlugosci -3
                 if self.descrambledsygnal.signal[i] == 1:
                     j += 1
-                if j % 2 == 0 and self.descrambledsygnal.voltage[i] == 'B': #maska byla B00V wiec wracamy zera na pozycjach B oraz V = i, i+3
-                    self.descrambledsygnal.voltage[i] = 'Z'
-                    self.descrambledsygnal.voltage[i+3] = 'Z'
-                    j = 0
-                elif j % 2 == 1 and self.descrambledsygnal.voltage[i] == 'V': #maska byla 000V bo jedynek nieparzysta liczba wiec wracamy zero na pozycji i
-                    self.descrambledsygnal.voltage[i] = 'Z'
-                    j = 0
+                    if j % 2 == 0 and self.descrambledsygnal.voltage[i] == 'B': #maska byla B00V wiec wracamy zera na pozycjach B oraz V = i, i+3
+                        self.descrambledsygnal.voltage[i] = 'Z'
+                        self.descrambledsygnal.voltage[i+3] = 'Z'
+                        j = 0
+                    elif j % 2 == 1 and self.descrambledsygnal.voltage[i] == 'V': #maska byla 000V bo jedynek nieparzysta liczba wiec wracamy zero na pozycji i
+                        self.descrambledsygnal.voltage[i] = 'Z'
+                        j = 0
                 i += 1
         elif algorithm == "B8ZS":
             i = 0
@@ -26,6 +26,7 @@ class Descrambler:
                     self.descrambledsygnal.voltage[i + 3] = 'Z'
                     self.descrambledsygnal.voltage[i + 4] = 'Z'
                 i += 1
+        self.toZeroOneRepresentation()
         return self.descrambledsygnal
     
     def toZeroOneRepresentation(self):
--- Project Files/Sygnal.py
+++ Project Files/Sygnal.py
@@ -1,19 +1,15 @@
-#Klasa przedstawiajaca sygnal AMI
+#Klasa reprezentujaca sygnal w postaci napieciowej oraz zero-jedynkowej
 class Sygnal:
     def __init__(self,signal):      #konstruktor klasy Sygnal
         self.signal = list(signal)  #postac zero-jedynkowa
         self.voltage = ['Z']*len(signal) #na poczatku sygnal jest wypelniony zerami - reprezentacja napieciowa
         counter = 0
-        lastvoltage = 'Z'       #zmienna opisujaca ostatnia zmiane napiecia 
         for i in self.signal:
             if i == 1 and counter == 0: #poczatek sygnalu
                 self.voltage[counter] = 'H'
-                lastvoltage = 'H'
-            elif i == 1 and lastvoltage == 'H':
+            elif i == 1 and self.voltage[counter-1]=='H':
                 self.voltage[counter] = 'L'
-                lastvoltage = 'L'
-            elif i == 1 and lastvoltage =='L':
+            elif i == 1:
                 self.voltage[counter] = 'H'
-                lastvoltage = 'H'
             counter +=1
     
